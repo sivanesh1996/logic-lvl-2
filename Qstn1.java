eda7bd0de36c0244f2c044b053756e6ee6ec3858
sivanesh1996 <111571530+sivanesh1996@users.noreply.github.com>
2022-08-21 21:25:18 +0530

    Add files via upload
    A boy has a certain amount and he spends 1/3 of the amount in movie, another 1/3 of amount in juice shop and another 1/3 of amount in mall.What amount does the boy have inititally?

---
+++ Qstn1.java
@@ -0,0 +1,18 @@
+class Qstn1
+{
+    
+    public static void main(String[] args)
+    {
+       int c_bal=8;
+       for(int i=1;i<=3;i++)
+      {
+       int exp=c_bal/2;
+       c_bal=c_bal+exp;
+       System.out.println(c_bal +" Rs");
+       }    
+    }
+}
+
+      
+    
+

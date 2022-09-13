02e392fc8aa0bf6cac44a9b29bc169030dbdcba8
sivanesh1996 <vssivanesh@gmail.com>
2022-09-13 14:50:39 +0530

    Maths class-some fields and methods

---
+++ Maths.java
@@ -0,0 +1,21 @@
+class Maths
+{
+    public static void main(String[] args)
+      {
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+        System.out.println(Math.abs(-2.222));
+        System.out.println(Math.abs(-2.2));
+        System.out.println(Math.abs(-2));
+        System.out.println(Math.abs(-55555555));
+        System.out.println(Math.addExact(5,9));
+        System.out.println(Math.cbrt(8));
+        System.out.println(Math.ceil(9.0001));
+        System.out.println(Math.copySign(9.1,-1.0));
+        System.out.println(Math.floor(2.9));
+        System.out.println(Math.floorDiv(5,4));
+        System.out.println(Math.floorMod(5,3));
+        System.out.println(Math.max(8,7));
+        System.out.println(Math.pow(5,2));
+       }
+}

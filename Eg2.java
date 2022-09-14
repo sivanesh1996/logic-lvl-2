f5db39a36d0ffbf9d92a3182fb2265213b9f2b8b
sivanesh1996 <vssivanesh@gmail.com>
2022-09-14 21:46:40 +0530

    basic-given values

---
+++ Eg2.java
@@ -0,0 +1,10 @@
+class Eg2
+{
+    public static void main(String[] args)
+      {
+        int Age[]={10,12,14};       // {index 0 ,index 1,index 2}
+        System.out.println(Age[2]); // Age[index]
+        System.out.println("The size of this array Age is:"+Age.length); //length variable in array returns the size of array not index
+        System.out.println("The index of this array Age is:"+(Age.length-1));  // length-1 is index
+     }
+}

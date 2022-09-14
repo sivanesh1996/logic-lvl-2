1a76fee3b2b444a9121dc3d318b5d4cf99209b9d
sivanesh1996 <vssivanesh@gmail.com>
2022-09-14 21:47:17 +0530

    basic-array size is known

---
+++ Eg1.java
@@ -0,0 +1,11 @@
+class Eg1
+{
+  public static void main(String[] args)
+    {
+       int Age[];
+       Age=new int[3]; //[3] is size
+      System.out.println(Age[0]);
+      Age[2]=5; //Age[2] means index
+      System.out.println(Age[2]);
+    }
+}

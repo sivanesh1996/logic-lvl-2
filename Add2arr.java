89ac14302fed1afea0dfe09b38d93cfeb0ba8d5a
sivanesh1996 <vssivanesh@gmail.com>
2022-09-15 22:18:48 +0530

    add 2 array

---
+++ Add2arr.java
@@ -0,0 +1,22 @@
+class Add2arr
+{
+ public static void main(String[] args)
+   {
+     int arr[]={ 1,1,1};
+                 
+
+      int arr1[]={ 2,2,2};
+                    
+       int arr2[]=new int[3];
+      
+    for(int i=0;i<arr.length;i++)
+     {
+      
+          arr2[i]=arr[i]+arr1[i];
+          System.out.print(arr2[i]);
+         }
+      }
+
+    
+}
+     
